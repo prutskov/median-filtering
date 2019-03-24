7fca7937b6a485588d795526dadde9f916bc2c29
prutskov <lehaprutskov@gmail.com>
2019-03-24 22:20:01 +0300

    Added func fo converting from cv::Mat to float pointer

--- CVHelper.cpp
+++ CVHelper.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 #include "CVHelper.h"
 
-
 CVHelper::CVHelper()
 {
 }
@@ -13,10 +12,33 @@ CVHelper::~CVHelper()
 
 void CVHelper::loadImage(std::string path, int flag)
 {
-	_loadedImage = imread(path, flag);   
+	_loadedImage = imread(path, flag);
+	auto frame = convertToPtr(_loadedImage);
 }
 
 void CVHelper::imageShow()
 {
 	imshow("Original image", _loadedImage);
+}
+
+Frame CVHelper::convertToPtr(Mat data)
+{
+	const int nRows = data.rows;
+	const int nCols = data.cols;
+
+	Frame frame(nRows, nCols, 
+		std::shared_ptr<float[]>(new float[nRows*nCols], std::default_delete<float[]>()));
+
+	Mat_<float> floatData;
+	data.convertTo(floatData, CV_32F);
+	
+	for (int i = 0; i < nRows; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			frame.dataPtr[i*nCols + j] = floatData.ptr<float>(i)[j];
+		}
+	}
+
+	return frame;
 }
--- CVHelper.h
+++ CVHelper.h
@@ -3,17 +3,41 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <memory>
 
 using namespace cv;
+
+struct Frame
+{
+	Frame() : nRows(0), nCols(0)
+	{
+		dataPtr = std::shared_ptr<float[]>(nullptr);
+	}
+	Frame(const int rows, const int cols, std::shared_ptr<float[]> data) : nRows(rows), nCols(cols)
+	{
+		dataPtr = data;
+	}
+
+	std::shared_ptr<float[]> dataPtr;
+	int nRows;
+	int nCols;
+};
+
 class CVHelper
 {
 public:
 	CVHelper();
 	virtual ~CVHelper();
-	void loadImage(std::string path, int flag = IMREAD_COLOR);
+	void loadImage(std::string path, int flag = IMREAD_GRAYSCALE);
 	void imageShow();
 
 private:
-	cv::Mat _loadedImage;
+	Frame convertToPtr(Mat data);
+
+private:
+	Mat _loadedImage;
+	Frame _imagePtr;
 };
+
+
 
